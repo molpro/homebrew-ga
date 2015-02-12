76b8f4028785696983fb2ea4d8c62914a7448de1
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2015-02-12 20:10:04 +0000

    ga 5.3 (new formula)
    Add the Global Arrays (GA) toolkit that provides an efficient and portable "shared-memory" programming interface for distributed-memory computers.

---
+++ ga.rb
@@ -0,0 +1,59 @@
+class Ga < Formula
+  homepage "http://hpc.pnl.gov/globalarrays/"
+  url "http://hpc.pnl.gov/globalarrays/download/ga-5-3.tgz"
+  version "5.3"
+  sha1 "b4e5fdc7f25e76d4d9e4c8b23f9c9ae2855cd925"
+
+  depends_on "gcc"
+  depends_on :fortran
+  depends_on "mpich-ga"
+
+  def install
+    ENV["PATH"] = "#{HOMEBREW_PREFIX}/bin:" + ENV["PATH"]
+
+    system "./configure",
+           "CC=mpicc",
+           "FC=mpif90",
+           "F77=mpif77",
+           "CXX=mpicxx",
+           "MPICC=mpicc",
+           "MPIFC=mpif90",
+           "MPIF77=mpif77",
+           "MPICXX=mpicxx",
+           "--enable-cxx",
+           "--prefix=#{prefix}"
+    system "make"
+    #system "make check MPIEXEC='mpiexec -np 2'"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"hello.c").write <<-EOS.undent
+      #include <mpi.h>
+      #include <stdio.h>
+
+      int main()
+      {
+        int size, rank, nameLen;
+        int testpattern;
+        int pattern=999;
+        char name[MPI_MAX_PROCESSOR_NAME];
+        MPI_Init(NULL, NULL);
+        GA_Initialize();
+        MPI_Comm_size(MPI_COMM_WORLD, &size);
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        MPI_Get_processor_name(name, &nameLen);
+        testpattern=pattern;
+        GA_Brdcst(&testpattern,sizeof(testpattern),0);
+        printf("[%d/%d] Hello, world! My name is %s; broadcast error=%d.\\n", rank, size, name,testpattern-pattern);
+        GA_Terminate();
+        MPI_Finalize();
+        return testpattern != pattern;
+      }
+    EOS
+    ldflags = `#{bin}/ga-config --ldflags --libs`
+    system("mpicc hello.c -o hello "+ldflags)
+    system "./hello"
+    system "mpirun", "-np", "4", "./hello"
+  end
+end
